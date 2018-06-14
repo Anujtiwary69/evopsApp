f0178778f465c3647426c63c5507b6fd0c517a7e
Anujtiwary69 <anujtiwary69@gmail.com>
2018-06-14 14:21:40 +0400

    Initial commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2018 <a href="https://www.evops-pr.com">Evops Marketing and PR</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 1.00
+    </div>
+  </footer>
---
+++ resources/views/sliderbar.blade.php
@@ -0,0 +1,17 @@
+<nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          
+          <li class="nav-item">
+            <a href="/add/user" class="nav-link">
+              <i class="nav-icon fa fa-th"></i>
+              <p>
+                Add Users
+                <span class="right badge badge-danger">New</span>
+              </p>
+            </a>
+          </li>
+          
+        </ul>
+      </nav>
